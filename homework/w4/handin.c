90fa5bedb603a442267abdf328ae769cfe1787a3
gly11 <gly-11@foxmail.com>
2022-03-23 19:42:05 +0800

    # v0.2.2，优化了作业代码

--- homework/w4/handin.c
+++ homework/w4/handin.c
@@ -11,6 +11,10 @@ int max(int a, int b, int c) {
     return ( a>b && a > c) ? a : ( (b>c && b>a) ? b : c);
 }
 
+void split(char a, int x, int i){
+    i < x ? ( putchar(a), split(a, x, ++i) ) : putchar('\n');
+}
+
 void e3_6( ) {
   char str[6] = "China";
   char *p = str;
@@ -47,7 +51,9 @@ void e4_4( )
 int main(int argc, char const *argv[])
 {
     e3_6();
+    split('-', 80, 0);
     e3_8();
+    split('-', 80, 0);
     e4_4();
     return 0;
 }
